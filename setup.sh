9c744f4a33968e3766f046a8c21377f45fd3cf24
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-02 00:07:41 +0200

    Always ask where to deploy the scripts and fixed finding the scripts in the 'scripts' subfolder

--- setup.sh
+++ setup.sh
@@ -30,10 +30,25 @@ UNZIP_PATH="$DIR_PATH/$FOLDER_NAME"
 SCRIPT_PATH="$UNZIP_PATH/terminawake.sh"
 
 # Unzip the located zip file
-unzip "$TARGET_PATH" -d "$DIR_PATH"
+unzip -o "$TARGET_PATH" -d "$DIR_PATH"
 
 # Make the scripts executable
 chmod +x "$SCRIPT_PATH"
 for script in "$UNZIP_PATH"/scripts/*.sh; do
     chmod +x "$script"
 done
+
+# Ask the user to choose a directory
+echo "Where do you want to move TerminAWake?"
+echo "1. Home directory"
+echo "2. Downloads directory"
+echo "3. Applications directory"
+read -p "Enter your choice (1-3): " choice
+
+# Move the folder to the selected directory
+case $choice in
+    1) mv "$UNZIP_PATH" ~/ ;;
+    2) mv "$UNZIP_PATH" ~/Downloads ;;
+    3) sudo mv "$UNZIP_PATH" /Applications ;;
+    *) echo "Invalid choice. Exiting."; exit 1 ;;
+esac
