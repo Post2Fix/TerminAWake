8da61f498337907fa7ba6b5ef307d51f652f25e4
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-01 20:41:01 +0200

    Reverted terminawake.sh

--- terminawake.sh
+++ terminawake.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-# terminawake.sh
-echo "Starting TerminAwake..."
+# The directory where the helper scripts are located
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/scripts"
 
-# Path of the script directory
-SCRIPT_DIR=$(dirname "$0")
+# Include the helper scripts
+source "$SCRIPTS_DIR/sleep_control.sh"
+source "$SCRIPTS_DIR/sleep_duration.sh"
+source "$SCRIPTS_DIR/sleep_abort.sh"
 
-# Set duration
-"${SCRIPT_DIR}/sleep_duration.sh" "$1"
+disable_indefinitely() {
+    # Prevent sleep indefinitely for battery and AC power modes when no argument was passed
+    disable_sleep
 
-# Disable sleep
-"${SCRIPT_DIR}/sleep_control.sh" disable
+    echo "Sleep disabled indefinitely. Press Enter to re-enable sleep."
+    # Wait for user to exit the program to reenable sleep
+    read -r
 
-# Start listening for user input
-"${SCRIPT_DIR}/listener.sh"
+    # Revert to the default sleep behavior
+    enable_sleep
+}
 
-echo "TerminAwake has stopped."
+disable_temporarily() {
+    local duration_sec=$(set_duration "$1")
+
+    # Prevent sleep for the specified duration
+    disable_sleep
+
+    # Sleep for the specified duration
+    echo "Sleep disabled for $1. Press Enter to re-enable sleep."
+    abort_sleep "$duration_sec" || true
+
+    # Revert to the default sleep behavior
+    enable_sleep
+}
+
+# Check if an argument was passed
+if [[ -z $1 ]]; then
+    disable_indefinitely
+else
+    disable_temporarily "$1"
+fi
