295066b2075ef48898469da88dcd268c721e87dd
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-01 19:24:37 +0200

    Revert to working version

--- scripts/sleep_duration.sh
+++ scripts/sleep_duration.sh
@@ -1,11 +1,62 @@
 #!/bin/bash
+source "$(dirname "$0")/sleep_control.sh"
 
-# sleep_duration.sh
-echo "The system will stay awake for $1."
+# Disable sleep
+disable_sleep
 
-# Set sleep duration
-sudo pmset sleep 0 displaysleep 0 &>/dev/null
-sudo pmset -a disablesleep 1 &>/dev/null
+# Start a background job which re-enables sleep after the specified duration
+sleep "$1" && enable_sleep &
 
-# Sleep timer
-(sleep "$1" && "${SCRIPT_DIR}/sleep_control.sh" enable) &
+sleep_pid=$!
+
+# Function to turn display off
+function display_off {
+    echo "Turning display off..."
+    osascript -e 'tell application "Finder" to sleep'
+}
+
+# Function to turn display on
+function display_on {
+    echo "Waking display up..."
+    pmset displaysleepnow
+}
+
+# Function to lock screen
+function lock_screen {
+    echo "Locking screen..."
+    "/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession" -suspend
+}
+
+# Configure terminal to not wait for Enter key
+stty -icanon -echo min 1 time 0
+
+echo "Sleep disabled for $1. Press Return to re-enable sleep, 'D' to turn display on/off, 'L' to lock screen."
+
+while true
+do
+    read key
+    case "$key" in
+    "")
+        # If Enter is pressed, re-enable sleep and break the loop
+        echo "Re-enabling sleep..."
+        kill "$sleep_pid"
+        enable_sleep
+        break
+        ;;
+    [dD])
+        # If 'D' is pressed, toggle display on/off
+        echo "Toggling display..."
+        display_off
+        sleep 1
+        display_on
+        ;;
+    [lL])
+        # If 'L' is pressed, lock screen
+        echo "Locking screen..."
+        lock_screen
+        ;;
+    esac
+done
+
+# Reset terminal configuration
+stty sane
