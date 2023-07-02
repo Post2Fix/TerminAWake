49c1d9a52a395d5543fbc6e27e5b70699c7d2271
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-03 00:38:44 +0200

    Fixed locating the other scripts in the subdirectory

--- scripts/sleep_abort.sh
+++ scripts/sleep_abort.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Get the directory where the helper scripts are located
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# Include the sleep_control script
+source "$SCRIPTS_DIR/sleep_control.sh"
+
 # Function to abort sleep
-# Takes duration in seconds as argument
 abort_sleep() {
     local duration_sec=$1
-    sleep "$duration_sec"  # Pauses the script for the specified duration
+    sleep "$duration_sec" & wait $!
 }
