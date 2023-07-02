9157b46ce6bc26d112e5551f08cd2303feadab4c
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-02 23:53:23 +0200

    Update sleep_duration.sh

--- scripts/sleep_duration.sh
+++ scripts/sleep_duration.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+# Function to set sleep duration
+# Takes duration as argument
 set_duration() {
     local duration=$1
-    # Separate the time & timescale suffixes
-    local suffix="${duration: -1}"
-    local value="${duration%?}"
+    local suffix="${duration: -1}"  # Get the last character
+    local value="${duration%?}"  # Get all characters except the last
 
-    # Identify user input's timescale and convert it to seconds
     case "$suffix" in
-    s) duration_sec="$value" ;;
-    h) duration_sec=$((value * 3600)) ;;
-    *) duration_sec="$((duration * 60))" ;;  # Assume minutes suffix other than 'h' & 's' (including no value)
+    s) duration_sec="$value" ;;  # If suffix is 's', interpret as seconds
+    h) duration_sec=$((value * 3600)) ;;  # If suffix is 'h', interpret as hours and convert to seconds
+    *) duration_sec="$((duration * 60))" ;;  # Else, interpret as minutes and convert to seconds
     esac
 
-    echo $duration_sec
+    echo $duration_sec  # Print the duration in seconds
 }
