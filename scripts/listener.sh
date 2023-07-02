13db23f9dca37d03681be0fcbc8a7e043d1643a4
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-02 23:51:27 +0200

    Update listener.sh

--- scripts/listener.sh
+++ scripts/listener.sh
@@ -3,35 +3,30 @@
 # Include the display control script
 source "$(dirname "$0")/display_control.sh"
 
-# listener.sh
-
+# Function to listen to user commands
 listen_commands() {
-    stty -icanon -echo min 1 time 0
+    stty -icanon -echo min 1 time 0  # Change terminal settings to enable immediate character input
 
-    while true; do
-        read -r key
+    while true; do  # Run indefinitely
+        read -r key  # Read a single character from user input
         case "$key" in
-            d)
-                # Sleep the display
-                sleep_display
+            d)  # If 'd', sleep the display
+                sleep_display  # Calls the function from display_control.sh
                 ;;
-            l)
-                # pmset does not have an equivalent for locking the screen
+            l)  # If 'l', there is no pmset equivalent for locking the screen
                 ;;
-            r)
-                # Enabling sleep
-                enable_sleep
+            r)  # If 'r', enable system sleep
+                enable_sleep  # Calls the function from sleep_control.sh
                 ;;
-            q)
+            q)  # If 'q', break the loop
                 echo "Quitting..."
                 break
                 ;;
-            *)
+            *)  # If anything else, print "Unknown command."
                 echo "Unknown command."
                 ;;
         esac
     done
 
-    # reset terminal settings
-    stty sane
+    stty sane  # Reset terminal settings to default
 }
