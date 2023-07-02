62e19197f809a42921eaca2a24109ca7b6e4a132
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-02 23:52:03 +0200

    Update sleep_abort.sh

--- scripts/sleep_abort.sh
+++ scripts/sleep_abort.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# Function to abort sleep
+# Takes duration in seconds as argument
 abort_sleep() {
     local duration_sec=$1
-    sleep "$duration_sec"
+    sleep "$duration_sec"  # Pauses the script for the specified duration
 }
