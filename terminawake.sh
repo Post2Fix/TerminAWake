53665c38a3154beca3f7fe0d77a8d74eb3eb2ad5
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-02 23:40:55 +0200

    Update terminawake.sh

--- terminawake.sh
+++ terminawake.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+# Check if the script is run with sudo
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 # The directory where the helper scripts are located
 SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/scripts"
 
 # Include the helper scripts
 source "$SCRIPTS_DIR/sleep_control.sh"
 source "$SCRIPTS_DIR/sleep_duration.sh"
+source "$SCRIPTS_DIR/display_control.sh"
+source "$SCRIPTS_DIR/listener.sh"
 source "$SCRIPTS_DIR/sleep_abort.sh"
 
 disable_indefinitely() {
     # Prevent sleep indefinitely for battery and AC power modes when no argument was passed
     disable_sleep
 
-    echo "Sleep disabled indefinitely. Press Enter to re-enable sleep."
-    # Wait for user to exit the program to reenable sleep
-    read -r
+    echo "Sleep disabled indefinitely. Press 'd' to toggle display, 'l' to lock screen, 'r' to enable sleep, or 'q' to quit."
+    listen_commands
 
     # Revert to the default sleep behavior
     enable_sleep
@@ -26,9 +33,8 @@ disable_temporarily() {
     # Prevent sleep for the specified duration
     disable_sleep
 
-    # Sleep for the specified duration
-    echo "Sleep disabled for $1. Press Enter to re-enable sleep."
-    abort_sleep "$duration_sec" || true
+    echo "Sleep disabled for $1. Press 'd' to toggle display, 'l' to lock screen, 'r' to enable sleep, or 'q' to quit."
+    listen_commands
 
     # Revert to the default sleep behavior
     enable_sleep
