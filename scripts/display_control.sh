746dc72a06f756757456c0bdd7ff268f74954538
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-03 04:09:05 +0200

    Update display_control.sh

--- scripts/display_control.sh
+++ scripts/display_control.sh
@@ -3,6 +3,6 @@
 # Function to sleep the display
 # pmset displaysleepnow forces the display to immediately enter sleep mode
 sleep_display() {
-    echo "Turnning off the display..."
+    #echo "Turnning off the display..."
     sudo pmset displaysleepnow
 }
