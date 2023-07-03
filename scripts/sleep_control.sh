ad33065898fb9b4ded0b409a73ee249312e787cf
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-03 14:37:41 +0200

    Created a separate function to manage sleep delay timers

--- scripts/sleep_control.sh
+++ scripts/sleep_control.sh
@@ -21,3 +21,19 @@ toggle_sleep() {
         echo -e "\r\nSleep disabled."
     fi
 }
+
+# Define a function to set sleep timers
+set_timers() {
+    echo -e "\nEnter system sleep delay (in format 'NhMmSs', e.g. '1h30m' or '45s'): "
+    read -r system_sleep_time
+    echo -e "Enter display sleep delay (in format 'NhMmSs', e.g. '1h30m' or '45s'): "
+    read -r display_sleep_time
+
+    system_sleep_delay=$(echo "$system_sleep_time" | awk -F'[hms]' '{ print ($1*3600)+($2*60)+$3 }')
+    display_sleep_delay=$(echo "$display_sleep_time" | awk -F'[hms]' '{ print ($1*3600)+($2*60)+$3 }')
+
+    # Now use pmset to set the sleep and display sleep delays, replacing any existing ones
+    sudo pmset -a sleep $system_sleep_delay
+    sudo pmset -a displaysleep $display_sleep_delay
+    echo -e "\nSleep timers set: system sleep delay of $system_sleep_time and display sleep delay of $display_sleep_time."
+}
