76ad4b7a6f04633e54aa3f05d098a71b7655533f
Post2Fix <127422443+Post2FixO@users.noreply.github.com>
2023-07-05 15:33:39 +0200

    Improved listener.sh input method, outputs and more (details in extended description)
    - Implement new input method that requires return key submission and allows delete key erasure (editing) suring input editing.
    - Set terminal to non-canonical mode removed (error prone) since live input is replaced with safer return to submit input (needed for time submission and added for all inputs). At program end, original settings no longer need to be restored
    - Program options switch to numbered options instead of letters
    - Output texts and code comments improve
    - Script is shortened

--- scripts/listener.sh
+++ scripts/listener.sh
@@ -8,48 +8,36 @@ source "$SCRIPTS_DIR/display_control.sh"
 source "$SCRIPTS_DIR/sleep_control.sh"
 
 # Instruction string
-instructions="'d' - turn off display, 'l' - lock screen, 'r' - toggle sleep, 'q' - enable sleep and quit."
+instructions="Select option: 1. turn off display, 2. lock screen instructions, 3. toggle sleep, 4. set sleep delay, 5. enable sleep & quit."
 
 # Function to listen to user commands
 listen_commands() {
-    
-    # Setting terminal to non-canonical mode
-    old_stty_cfg=$(stty -g)
-    stty raw -echo min 0
-
     echo "$instructions"
     # Loop for reading commands
-    while IFS= read -r -n 1 key; do # -n 1 reads one character at a time without waiting for a newline so the script responds immediately to keystroke. -r prevents backslashes from being interpreted as escape characters
+    while IFS= read -e -r key; do # read line by line with editing enabled (delete key deletes instead of printing '^?'). Press retunr to send commands
         case "$key" in
-            d)  # If 'd', sleep the display
-                sleep_display  # Calls the function from display_control.sh
+            1)  # Turn off the display
+                turn_off_display  # Calls the function from display_control.sh
                 echo -e "\r\nDisplay turned off. When the display turns off, your lock screen will activates based on the manual 'lock screen' system setting, 'Require password after...'. Setting it to 'immediately' will lock & secure your computer while it stays awake."
                 ;;
-            r)  # If 'r', toggle system sleep
+            2)  # Present screen locking instructions
+                echo -e "\r\nThe macOS setting 'Lock Screen Settings -> 'Require password sfter...' sets how long after the display turns off, a password is required. This can only be configured manually. Alternatively, pressing 'control + command + q' or the power button (sometimes multiple times...), will lock a Mac at any point"
+                ;;
+            3)  # Toggle sleep
                 toggle_sleep  # Calls the function from sleep_control.sh
                 ;;
-            t)  # If 't', request sleep timer delay input
-                echo -e "\r\nPlease enter sleep timer delay in minutes (or '0' for no delay):"
-                read -r delay
-                echo -e "\r\nPlease enter display sleep delay in minutes (or '0' for no delay):"
-                read -r display_delay
-                # Here, add code to apply these delays. You might need to modify terminawake.sh and create additional scripts to handle these values.
+            4)  # Set and display a new sleep delay
+                echo -e "\r\nSetting up sleep delay"
+                set_timers  # Calls the function from sleep_control.sh
                 ;;
-            l)  # If 'l', there is no pmset equivalent for locking the screen
-                echo -e "\r\n'control + command + q' will lock a Mac computer"
-                ;;
-            q)  # If 'q', break the loop
-                echo -e "\r\nEnabling sleep and quitting..."
+            5)  # Enable sleep and break the loop to quit the program
+                echo -e "\r\nEnabling sleep and quitting"
                 break
                 ;;
-            *)  # For unrecognized keystrokes, print "Unknown command."
+            *)  # For all other keystrokes, print "Unknown command."
                 echo -e "\r\nUnknown command."
                 ;;
         esac
-        sleep 1  # This can be adjusted as needed to hold the message on the screen
         echo -e "\r\n\n$instructions"
     done
-
-    # Restoring terminal settings
-    stty "$old_stty_cfg"
 }
